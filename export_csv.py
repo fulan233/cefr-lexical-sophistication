c106e59e2636aed1a79dfe2bab43713690139889
iris2hu <bnuhurenfen@126.com>
2024-10-07 00:09:52 +0800

    update the codes for the tool
    1. limit the computation of indices to alpha words only;
    2. add supports for different computing modes: LazyA1, AW, CW, Min.

--- export_csv.py
+++ export_csv.py
@@ -3,22 +3,26 @@ import re
 import pickle
 import pandas as pd
 import os
+import sys
 from math import sqrt
 
-with open("puncs.data", "rb") as f1:
-    punc = pickle.load(f1)
-nums = re.compile(r'[0-9]', re.S)
+with open("./dict/mono_pos.data", "rb") as f:
+    mono_pos_dict = pickle.load(f)
+
+# load level data
+with open('./dict/target_level.data', 'rb') as f:
+    ld = pickle.load(f)
 
 tags = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'UN']
 
-tag_score_dict={
-    'A1':1,
-    'A2':2,
-    'B1':3,
-    'B2':4,
-    'C1':5,
-    'C2':6,
-    'UN':0
+tag_score_dict = {
+    'A1': 1,
+    'A2': 2,
+    'B1': 3,
+    'B2': 4,
+    'C1': 5,
+    'C2': 6,
+    'UN': 0
 }
 
 plus_feature_type = {"A": ['A1', 'A2'],
@@ -26,69 +30,86 @@ plus_feature_type = {"A": ['A1', 'A2'],
                      "C": ['C1', 'C2'],
                      "AboveB1": ['B1', 'B2', 'C1', 'C2'],
                      "AboveB2": ['B2', 'C1', 'C2']
-                    }
+                     }
 
 POSTFIX = ["_type_ratio", "_type_root_ratio", "_token_ratio", "_token_root_ratio"]
 
 
-def extract_tag(sequence): 
+def extract_tag(sequence, lazy_mode=None):
     pure_tag_sequence = []
     for token in sequence:
         if "#" in token:
-            pure_tag_sequence.append(token.split("#")[1])
+            word = token.split('-')[0]
+            tag = token.split("#")[1]
+            ### for computing modes
+            if lazy_mode == 'LazyA1':
+                if word in ld and ld[word]['min'] == 'A1':
+                    tag = ld[word]['min']
+            elif lazy_mode == 'Rand':
+                if word in ld:
+                    tag = ld[word]['rand']
+            elif lazy_mode == 'Min':
+                if word in ld:
+                    tag = ld[word]['min']
 
+            pure_tag_sequence.append(tag)
         elif "_" in token:
-            tag = token.split("_")[1]
-            ttoken = token.split("_")[0]
-            if tag == "UN":
-                if ttoken in punc:
-                    continue
-                elif len(re.findall(nums, ttoken)) > 0:
-                    continue
-                else:
-                    pure_tag_sequence.append('UN')
-            else:
-                pure_tag_sequence.append(tag)
-        else:
-            print(token)
+            word, tag = token.split("_")
+            pure_tag_sequence.append(tag)
+
     return pure_tag_sequence
 
+def Is_Content_Token(token):
+
+    if "#" in token:
+        # word = token.split('-')[0]
+        # tag = token.split("#")[1]
+        pos = token.split('_')[0].split('-')[-1]
+        if pos in ['noun', 'verb', 'adjective', 'adverb']: # content words onnly
+            return True
+    elif "_" in token:
+        word, tag = token.split("_")
+        pos_tags = mono_pos_dict.get(word, 'NA')
+        if pos_tags[0] in ['noun', 'verb', 'adjective', 'adverb'] or pos_tags == 'NA': # for EVP unlisted words
+            return True
+
+    return False
 
 
-def mean_sophisitication_score(tag_sequence:list):
+def mean_sophistication_score(tag_sequence: list):
     total_score = 0
     for tag in tags:
         total_score += tag_sequence.count(tag) * tag_score_dict[tag]
     return total_score
 
 
-
-def output_csv(path, outputname):
+def output_csv(path, outfile, tag_mode):
     files = glob.glob(path)
     indices = {}
 
     for file in files:
         filename = os.path.split(file)[-1]
         feature_dict = {}
-        type_sequence = []  
-        all_sequence = []  
+        type_sequence = []
+        all_sequence = []
 
         for line in open(file, "r", encoding='UTF-8'):
             ll = line.strip().split(" ")
             for tag_token in ll:
+                if '#' not in tag_token and '_' not in tag_token:
+                    continue
+                # content words only
+                if tag_mode == 'CW' and not Is_Content_Token(tag_token):
+                    continue
                 all_sequence.append(tag_token)
                 if tag_token not in type_sequence:
                     type_sequence.append(tag_token)
 
-
         type_length, root_type_length = len(type_sequence), sqrt(len(type_sequence))
         amount, root_amount = len(all_sequence), sqrt(len(all_sequence))
 
-        type_pure_tags_sequence = extract_tag(type_sequence)  
-        amount_pure_tags_sequence = extract_tag(all_sequence)
-
-        # feature_dict['ntokens'] = amount
-        # feature_dict['ntypes'] = type_length
+        type_pure_tags_sequence = extract_tag(type_sequence, tag_mode)
+        amount_pure_tags_sequence = extract_tag(all_sequence, tag_mode)
 
         for tag in tags:
             feature_dict[tag + "_type_ratio"] = round((type_pure_tags_sequence.count(tag) / type_length), 3)
@@ -98,10 +119,10 @@ def output_csv(path, outputname):
 
         for compound_name, tag_sequence in plus_feature_type.items():
             for post in POSTFIX:
-                feature_dict[compound_name + post] = round(sum([feature_dict[k + post] for k in tag_sequence]),3)  
-        
-        feature_dict['token_mean_score'] = round(mean_sophisitication_score(amount_pure_tags_sequence)/amount,3)
-        feature_dict['type_mean_score'] = round(mean_sophisitication_score(type_pure_tags_sequence)/type_length,3)
+                feature_dict[compound_name + post] = round(sum([feature_dict[k + post] for k in tag_sequence]), 3)
+
+        feature_dict['token_mean_score'] = round(mean_sophistication_score(amount_pure_tags_sequence) / amount, 3)
+        feature_dict['type_mean_score'] = round(mean_sophistication_score(type_pure_tags_sequence) / type_length, 3)
 
         # update the EVP indices
         indices[filename] = feature_dict
@@ -109,10 +130,14 @@ def output_csv(path, outputname):
     # export the results to a csv file
     df_results = pd.DataFrame.from_dict(indices, orient='index')
     df_results.index.name = 'filename'
-    df_results.to_csv(outputname)
+    df_results.to_csv(outfile)
 
 
 if __name__ == '__main__':
-
-    output_csv('./output/*.txt', 'evp-indices.csv')
-
+    tag_mode = 'LazyA1'  # Default setting
+    if len(sys.argv) > 1:
+        tag_mode = sys.argv[1]
+        if tag_mode not in ['AW', 'CW', 'Min', 'LazyA1']:
+            print(f'Mode [{tag_mode}] not supported, Please use: AW/CW/Min/LazyA1...')
+            exit()
+    output_csv('./output/*.txt', f'EVP_indices_{tag_mode}.csv', tag_mode)
--- tag_text_server.py
+++ tag_text_server.py
@@ -1,13 +1,11 @@
 import glob
 from tqdm import tqdm
-from tabnanny import check
 from bert_serving.client import BertClient
 from tag_sense import *
 from utils import *
-# tag_info_ops = ""
 
 bc = BertClient()
- 
+
 def tag_text(infile, outfile):
     token_count_total = 0
     # step 1. text preprocessing
@@ -24,19 +22,19 @@ def tag_text(infile, outfile):
                 check_wordlist.append(abbreconvert(word))
             else:
                 check_wordlist.append(word)
-        wps = {check_wordlist[i]:poslist[i] for i in range(len(check_wordlist))}
+        wps = {check_wordlist[i]: poslist[i] for i in range(len(check_wordlist))}
         newsent = ' '.join(check_wordlist)
         if len(wps) >= 1:
-            keep_info.append([line_num, newsent, wps]) # for reliable contextual representations, we only tag sentences with >= 5 tokens
-                                                       # for EVP tranning, remove this restriction. hunan edited.
-    # step 2. generate contextualized embeddings for the sentences
+            keep_info.append([line_num, newsent, wps])
+            # for reliable contextual representations, we only tag sentences with >= 5 tokens
+            # for EVP tranning, remove this restriction. hunan edited.
 
+    # step 2. generate contextualized embeddings for the sentences
     input_sents = [k[1] for k in keep_info]
     vec = bc.encode(input_sents, show_tokens=True)
     all_arrays, all_tokens = vec
 
     # step 3. tag the sentences based on dictionary information
-
     label_result = {}
 
     for sid in range(len(all_tokens)):
@@ -45,13 +43,13 @@ def tag_text(infile, outfile):
         tokens = all_tokens[sid]
         token_count_total += len(tokens)
         arrays = all_arrays[sid]
-        label_result[line_num] = {'tokens':[], 'labels':[]}
+        label_result[line_num] = {'tokens': [], 'labels': []}
         token_id = -1
 
         # tag each token in a sentence
         for i in range(len(tokens)):
             token = tokens[i]
-            if token in ['[CLS]','[SEP]']:
+            if token in ['[CLS]', '[SEP]']:
                 continue
             elif token.startswith('##'):
                 label_result[line_num]['tokens'][-1] += token.replace('##', '')
@@ -61,50 +59,34 @@ def tag_text(infile, outfile):
             label_result[line_num]['tokens'].append(token)
             pos_tag = wps.get(token, 'None')
             token_lm = lemmatize(token, pos_tag)
-            
-            #优先义项标注 or 单义标注
+
+            # mono word first
             if token in mono_dict.keys():
                 CEFR_level = mono_dict.get(token)
-                label_result[line_num]['labels'].append([token_id,token+"_"+CEFR_level])
-            
+                label_result[line_num]['labels'].append([token_id, token + "_" + CEFR_level])
+
             elif token_lm in mono_dict.keys():
                 CEFR_level = mono_dict.get(token_lm)
-                label_result[line_num]['labels'].append([token_id,token_lm+"_"+CEFR_level])
+                label_result[line_num]['labels'].append([token_id, token_lm + "_" + CEFR_level])
 
-
-            elif token in target_words or token_lm in target_words:                
+            elif token in target_words or token_lm in target_words:
                 emb = arrays[i]
                 if token_lm in target_words:
                     token = token_lm  # lemma first, token second
 
-                sense_tag = tagSenseFromWordDict(token, pos_tag, emb, False)    # with or without POS information
-                if sense_tag:
-                    sense_id, simi = sense_tag
-                    label_result[line_num]['labels'].append([token_id, sense_id])
-            
-                
-            elif token in target_words or token_lm in target_words:                
-                emb = arrays[i]
-                if token_lm in target_words:
-                    token = token_lm  # lemma first, token second
+                sense_tag = tagSenseFromWordDict(token, pos_tag, emb, False)  # with or without POS information
+                # sense_tag = tagSensePosOnly(token, pos_tag) # exp suggested by R2
 
-                sense_tag = tagSenseFromWordDict(token, pos_tag, emb, False)    # with or without POS information
                 if sense_tag:
                     sense_id, simi = sense_tag
                     label_result[line_num]['labels'].append([token_id, sense_id])
-                    
-                    
-            # elif token != token_lm:
-            #     label_result[line_num]['labels'].append([token_id, token_lm])
-            
-            else:
-                label_result[line_num]['labels'].append([token_id, token+'_'+'UN'])
-            
-            
+
+            # modified to limit the token to alpha words only
+            elif token.isalpha():
+                label_result[line_num]['labels'].append([token_id, token + '_' + 'UN'])
 
     # step 4. save the tagged result
-
-    with open(outfile, 'w',encoding='utf-8') as f:
+    with open(outfile, 'w', encoding='utf-8') as f:
         for line_num, line in enumerate(all_sents):
             if line_num not in label_result:
                 newline = tag_and_lem(line) + '\n'
@@ -118,10 +100,11 @@ def tag_text(infile, outfile):
                 newline = ' '.join(tokenlist) + '\n'
                 f.write(newline)
 
+
 if __name__ == '__main__':
     files = glob.glob('./samples/*.txt')
     print(len(files), 'files to be processed')
 
-    for file_num,infile in tqdm(enumerate(files)):
+    for file_num, infile in tqdm(enumerate(files)):
         outfile = infile.replace('samples', 'output')
         tag_text(infile, outfile)
